c8be099768391ab53c90d8bfe383623fd3727b53
Arta Abedi <Hey@artaabedi.com>
2024-03-11 03:20:16 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,80 @@
-function light(){document.body.classList.remove("dark-mode"),document.getElementById("light").style.display="none",document.getElementById("dark").style.display="block"}function dark(){document.body.classList.add("dark-mode"),document.getElementById("dark").style.display="none",document.getElementById("light").style.display="block"}function theme(){document.body.classList.toggle("dark-mode");let e=document.getElementsByClassName("label");for(let t=0;t<e.length;t++)e[t].classList.add("dark")}function navtoggle(){document.querySelector(".bar").classList.toggle("active")}var loadtime,typingEffect=new Typed(".multiText",{strings:["BCA Graduate","Web Developer","UI<span > / </span>UX Designer"],loop:!0,typeSpeed:100,backSpeed:80,backDelay:1500});function reveal(){for(var e=document.querySelectorAll(".reveal"),t=0;t<e.length;t++){var l,a=window.innerHeight;e[t].getBoundingClientRect().top<a-10?e[t].classList.add("active"):e[t].classList.remove("active")}}function loader(){loadtime=setTimeout(showPage,2e3)}function showPage(){document.getElementById("loader").style.display="none",document.getElementById("main").style.display="block",document.getElementById("nav").style.display="flex"}function showhome(){setTimeout(function e(){document.getElementById("home").classList.add("active")},2100)}window.addEventListener("scroll",reveal);const date=new Date,year=date.getFullYear();document.getElementById("copy").innerHTML=year;
+// ------------------------Theme Switch -----------------------
+
+        function light() {
+            document.body.classList.remove("dark-mode")
+            document.getElementById("light").style.display = "none"
+            document.getElementById("dark").style.display = "block"
+            
+        }
+        function dark() {
+            document.body.classList.add("dark-mode")
+            document.getElementById("dark").style.display = "none"
+            document.getElementById("light").style.display = "block"
+
+        }
+
+        function theme() {
+            document.body.classList.toggle("dark-mode");
+            const label = document.getElementsByClassName("label");
+            for (let i = 0; i < label.length; i++) {
+                label[i].classList.add("dark")
+            }
+        }
+        function navtoggle() {
+            document.querySelector(".bar").classList.toggle("active")
+        }
+
+    
+// ----------------------multi Tag line---------------------------------------
+
+        var typingEffect = new Typed(".multiText", {
+
+            strings: ["BCA Graduate","Web Developer","UI<span > / </span>UX Designer"],
+
+            loop: true,
+
+            typeSpeed: 100,
+
+            backSpeed: 80,
+
+            backDelay: 1500
+        });
+
+// ---------------------------Scroll Animation--------------------------------
+
+    window.addEventListener('scroll', reveal);
+    function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){ 
+        var windowheight=window.innerHeight; 
+        var revealtop=reveals[i].getBoundingClientRect().top; 
+        var revealpoint=10; 
+        if(revealtop < windowheight - revealpoint) {
+        reveals[i].classList.add('active'); 
+    }else{ 
+        reveals[i].classList.remove('active'); 
+    } } } 
+
+
+//   ----------------------------Loader Animation ---------------------------------  
+
+            var loadtime;
+
+            function loader() {
+                loadtime = setTimeout(showPage, 2000);
+            }
+
+            function showPage() {
+                document.getElementById("loader").style.display = "none";
+                document.getElementById("main").style.display = "block";
+                document.getElementById("nav").style.display = "flex";
+            }
+            function showhome(){
+                var ld=setTimeout(shhome,2100)
+                function shhome(){
+                document.getElementById("home").classList.add('active');
+            }}
+
+            const date = new Date()
+                const year = date.getFullYear()
+                document.getElementById("copy").innerHTML = year
