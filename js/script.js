f71e1d73d7db190272b7260f7d2139177f161b2a
Arta Abedi <Hey@artaabedi.com>
2024-03-20 07:10:03 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,6 +40,7 @@
             backDelay: 1500
         });
 
+
 // ---------------------------Scroll Animation--------------------------------
 
     window.addEventListener('scroll', reveal);
@@ -77,4 +78,42 @@
 
             const date = new Date()
                 const year = date.getFullYear()
+
                 document.getElementById("copy").innerHTML = year
+
+
+//   ----------------------------Top Scroll ---------------------------------  
+
+   const showOnPx = 100;
+   const backToTopButton = document.querySelector(".back-to-top");
+   const pageProgressBar = document.querySelector(".progress-bar");
+
+   const scrollContainer = () => {
+   return document.documentElement || document.body;
+   };
+
+   const goToTop = () => {
+   document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+  };
+
+  document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+  });
+
+  backToTopButton.addEventListener("click", goToTop);
