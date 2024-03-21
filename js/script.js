f138a06822f347ee271efa59a51239f9fe7e09bf
Arta Abedi <Hey@artaabedi.com>
2024-03-22 01:45:05 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -127,9 +127,3 @@ if (scrollTopBtn) {
 }
 
 
-//   ----------------------------Online Support---------------------------------
-
-   <script type="text/javascript">
-  ["keydown","touchmove","touchstart","mouseover"].forEach(function(v){window.addEventListener(v,function(){if(!window.isGoftinoAdded){window.isGoftinoAdded=1;var i="kQC7uY",d=document,g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.type="text/javascript",g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}})});
-</script>
-
