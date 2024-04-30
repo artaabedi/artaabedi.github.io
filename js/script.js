e542823fdb940d8f705c991413f650da05bb326f
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-04-30 12:20:52 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,41 +59,33 @@
 
 //   ----------------------------Loader Animation---------------------------------  
 
-            var loadtime;
+function changeLanguage(languageCode) {
+    Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
+        if (elem.classList.contains('lang-' + languageCode)) {
+             elem.style.display = 'initial';
+        }
+        else {
+             elem.style.display = 'none';
+        }
+    });
+}
 
-            function loader() {
-                loadtime = setTimeout(showPage, 1000);
-            }
-
-            function showPage() {
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("main").style.display = "block";
-                document.getElementById("nav").style.display = "flex";
-            }
-            function showhome(){
-                var ld=setTimeout(shhome,1100)
-                function shhome(){
-                document.getElementById("home").classList.add('active');
-            }}
-
-            const date = new Date()
-                const year = date.getFullYear()
-
-                document.getElementById("copy").innerHTML = year
-
-
-//   ----------------------------Scroll Top---------------------------------  
-const btnChangeLanguage = document.getElementById('btnChangeLanguage');
-const html = document.documentElement;
-
-btnChangeLanguage.addEventListener('click', () => {
-  if (html.lang === 'en') {
-    html.lang = 'fa';
-    btnChangeLanguage.textContent = 'تغییر زبان به انگلیسی';
-  } else {
-    html.lang = 'en';
-    btnChangeLanguage.textContent = 'تغییر زبان به فارسی';
-  }
+// select handler
+const selector = document.getElementById('langSelector');
+selector.addEventListener('change', function (evt) {
+    changeLanguage(this.value);
 });
+
+// detect initial browser language
+const lang = navigator.userLanguage || navigator.language || 'en-EN';
+const startLang = Array.from(selector.options).map(opt => opt.value).find(val => lang.includes(val)) || 'en';
+changeLanguage(startLang);
+
+// updating select with start value
+selector.selectedIndex = Array.from(selector.options).map(opt => opt.value).indexOf(startLang)
+
+// fill "The selected language is:"
+document.getElementById('browserLang').innerText = lang;
+document.getElementById('startLang').innerText = startLang;
 
 
