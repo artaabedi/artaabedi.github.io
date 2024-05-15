84db4951e0c282b1c3d429e6948716d7d19b90e8
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-05-15 23:40:03 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@
             backSpeed: 80,
 
             backDelay: 1500
+                
         });
 
 
@@ -54,6 +55,7 @@
         reveals[i].classList.add('active'); 
     }else{ 
         reveals[i].classList.remove('active'); 
+  
     } } } 
 
 
@@ -124,37 +126,30 @@ if (scrollTopBtn) {
     scrollPos > offset ? scrollTopBtn.classList.add('is-active') : scrollTopBtn.classList.remove('is-active');
 
   }, false);
-}
+        
+ }
 
 
-<!-- -----------------------Effect----------------- -->
+//   -----------------------Effect-----------------
 
-particlesJS("particles-js",
-{"particles":{"number":{"value":150,"density":{"enable":false,"value_area":800}},
-"color":{"value":"#5a0eea"},
-"shape":{"type":"polygon","stroke":{"width":0,"color":"#000000"},
-"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
-"opacity":{"value":0.5,"random":true,
-"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
-"size":{"value":8,"random":true,"anim":{"enable":true,"speed":50,"size_min":0.1,"sync":true}},
-"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
-"move":{"enable":true,"speed":119.8265668111943,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":true,"rotateX":8000,"rotateY":3000}}},
-"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},
-"onclick":{"enable":true,"mode":"push"},
-"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
-"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
-"repulse":{"distance":200,"duration":0.4},
-"push":{"particles_nb":4},
-"remove":{"particles_nb":2}}},
-"retina_detect":false});
-var count_particles, stats, update; stats = new Stats; stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px'; 
-stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() { stats.begin();
-stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) 
-{ count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; }
-requestAnimationFrame(update); 
-}; 
-requestAnimationFrame(update);;
+/* ---- reset ---- */ 
+        
+body{ margin:0; font:normal 75% Arial, Helvetica, sans-serif; }
+
+canvas{ display: block; vertical-align: bottom; } 
+
+/* ---- particles.js container ---- */ 
+
+#particles-js{ position:absolute; width: 100%; height: 100%; background-color: #040c0e; background-image: url(""); background-repeat: ; background-size: Full; background-position: 50% 50%; } 
+
+/* ---- stats.js ---- */
+
+.count-particles{ background: #000022; position: absolute; top: 48px; left: 0; width: 80px; color: #13E8E9; font-size: .8em; text-align: left; text-indent: 4px; line-height: 14px; padding-bottom: 2px; font-family: Helvetica, Arial, sans-serif; font-weight: bold; }
+
+.js-count-particles{ font-size: 1.1em; } 
+
+#stats, .count-particles{ -webkit-user-select: none; margin-top: 5px; margin-left: 5px; }
+
+#stats{ border-radius: 3px 3px 0 0; overflow: hidden; } 
+
+.count-particles{ border-radius: 0 0 3px 3px; }
