d6f21a4c57aa9fd2cae44563364032aa006c29bb
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-05-16 00:56:50 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@
         }
 
     
-// ----------------------multi Tag line---------------------------------------
+// ----------------------Multi Tag Line---------------------------------------
 
         var typingEffect = new Typed(".multiText", {
 
@@ -57,32 +57,7 @@
    } } } 
 
 
-//   ----------------------------Loader Animation---------------------------------  
-
-            var loadtime;
-
-            function loader() {
-                loadtime = setTimeout(showPage, 1000);
-            }
-
-            function showPage() {
-                document.getElementById("loader").style.display = "none";
-                document.getElementById("main").style.display = "block";
-                document.getElementById("nav").style.display = "flex";
-            }
-            function showhome(){
-                var ld=setTimeout(shhome,1100)
-                function shhome(){
-                document.getElementById("home").classList.add('active');
-            }}
-
-            const date = new Date()
-                const year = date.getFullYear()
-
-                document.getElementById("copy").innerHTML = year
-
-
-//   ----------------------------Scroll Top---------------------------------  
+//  ----------------------------Scroll Top---------------------------------  
 
 const scrollTopBtn = document.querySelector('.js-scroll-top');
 if (scrollTopBtn) {
@@ -125,8 +100,3 @@ if (scrollTopBtn) {
 
   }, false);    
  }
-
-
-//  ------------------------Effect------------------------------
-
-particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#7f00ff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":false});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
