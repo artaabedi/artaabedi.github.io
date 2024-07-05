b7b490fed07f8d640a17d49657c35e92c29dd335
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-07-06 03:20:21 +0330

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,140 @@
+// ------------------------Theme Switch-----------------------
+
+        function light() {
+            document.body.classList.remove("dark-mode")
+            document.getElementById("light").style.display = "none"
+            document.getElementById("dark").style.display = "block"
+            
+        }
+        function dark() {
+            document.body.classList.add("dark-mode")
+            document.getElementById("dark").style.display = "none"
+            document.getElementById("light").style.display = "block"
+
+        }
+
+        function theme() {
+            document.body.classList.toggle("dark-mode");
+            const label = document.getElementsByClassName("label");
+            for (let i = 0; i < label.length; i++) {
+                label[i].classList.add("dark")
+            }
+        }
+        function navtoggle() {
+            document.querySelector(".bar").classList.toggle("active")
+        }
+
+    
+// ----------------------multi Tag line---------------------------------------
+
+        var typingEffect = new Typed(".multiText", {
+
+            strings: ["BCA Graduate","Web Developer","UI<span > / </span>UX Designer"],
+
+            loop: true,
+
+            typeSpeed: 100,
+
+            backSpeed: 80,
+
+            backDelay: 1500
+        });
+
+
+// ---------------------------Scroll Animation--------------------------------
+
+    window.addEventListener('scroll', reveal);
+    function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){ 
+        var windowheight=window.innerHeight; 
+        var revealtop=reveals[i].getBoundingClientRect().top; 
+        var revealpoint=10; 
+        if(revealtop < windowheight - revealpoint) {
+        reveals[i].classList.add('active'); 
+    }else{ 
+        reveals[i].classList.remove('active'); 
+    } } } 
+
+
+//   ----------------------------Loader Animation---------------------------------  
+
+            var loadtime;
+
+            function loader() {
+                loadtime = setTimeout(showPage, 1000);
+            }
+
+            function showPage() {
+                document.getElementById("loader").style.display = "none";
+                document.getElementById("main").style.display = "block";
+                document.getElementById("nav").style.display = "flex";
+            }
+            function showhome(){
+                var ld=setTimeout(shhome,1100)
+                function shhome(){
+                document.getElementById("home").classList.add('active');
+            }}
+
+            const date = new Date()
+                const year = date.getFullYear()
+
+                document.getElementById("copy").innerHTML = year
+
+
+//   ----------------------------Scroll Top---------------------------------  
+
+const scrollTopBtn = document.querySelector('.js-scroll-top');
+if (scrollTopBtn) {
+  scrollTopBtn.onclick = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
+  
+  const progressPath = document.querySelector('.scroll-top path');
+  const pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+  progressPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+  const updateProgress = function() {
+    const scroll = window.scrollY || window.scrollTopBtn || document.documentElement.scrollTopBtn;
+
+    const docHeight = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    );
+
+    const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    const height = docHeight - windowHeight;
+    var progress = pathLength - (scroll * pathLength / height);
+    progressPath.style.strokeDashoffset = progress;
+  }
+
+  updateProgress();
+  const offset = 100;
+
+  window.addEventListener('scroll', function(event) {
+    updateProgress();
+
+    //Scroll back to top
+    const scrollPos = window.scrollY || window.scrollTopBtn || document.getElementsByTagName('html')[0].scrollTopBtn;
+    scrollPos > offset ? scrollTopBtn.classList.add('is-active') : scrollTopBtn.classList.remove('is-active');
+
+  }, false);
+}
+
+
+//   --------------------NP-Process----------------------------
+
+NProgress.start(); // start    
+NProgress.set(0.4); // To set a progress percentage, call .set(n), where n is a number between 0..1
+NProgress.inc(); // To increment the progress bar, just use .inc(). This increments it with a random amount. This will never get to 100%: use it for every image load (or similar).If you want to increment by a specific value, you can pass that as a parameter
+NProgress.configure({ ease: 'ease', speed: 500 }); // Adjust animation settings using easing (a CSS easing string) and speed (in ms). (default: ease and 200)
+NProgress.configure({trickleSpeed: 800 }); //Adjust how often to trickle/increment, in ms.
+NProgress.configure({ showSpinner: false });//Turn off loading spinner by setting it to false. (default: true)
+NProgress.configure({ parent: '#container' });//specify this to change the parent container. (default: body)
+NProgress.done(); // end
+
