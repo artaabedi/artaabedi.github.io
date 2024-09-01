5abd8fceba3065d435ceb1d88e93f78920427a1b
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-09-01 23:42:33 +0330

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,200 +1,133 @@
+
 // ------------------------Theme Switch -----------------------
 
-function light() {
-	document.body.classList.remove("dark-mode");
-	document.getElementById("light").style.display = "none";
-	document.getElementById("dark").style.display = "block";
-}
-function dark() {
-	document.body.classList.add("dark-mode");
-	document.getElementById("dark").style.display = "none";
-	document.getElementById("light").style.display = "block";
-}
+        function light() {
+            document.body.classList.remove("dark-mode")
+            document.getElementById("light").style.display = "none"
+            document.getElementById("dark").style.display = "block"
+            
+        }
+        function dark() {
+            document.body.classList.add("dark-mode")
+            document.getElementById("dark").style.display = "none"
+            document.getElementById("light").style.display = "block"
 
-function theme() {
-	document.body.classList.toggle("dark-mode");
-	const label = document.getElementsByClassName("label");
-	for (let i = 0; i < label.length; i++) {
-		label[i].classList.add("dark");
-	}
-}
-function navtoggle() {
-	document.querySelector(".bar").classList.toggle("active");
-}
+        }
 
+        function theme() {
+            document.body.classList.toggle("dark-mode");
+            const label = document.getElementsByClassName("label");
+            for (let i = 0; i < label.length; i++) {
+                label[i].classList.add("dark")
+            }
+        }
+        function navtoggle() {
+            document.querySelector(".bar").classList.toggle("active")
+        }
+
+    
 // ----------------------multi Tag line---------------------------------------
 
-var typingEffect = new Typed(".multiText", {
-	strings: ["BCA Graduate", "Web Developer", "UI<span > / </span>UX Designer"],
+        var typingEffect = new Typed(".multiText", {
 
-	loop: true,
+            strings: ["BCA Graduate","Web Developer","UI<span > / </span>UX Designer"],
 
-	typeSpeed: 100,
+            loop: true,
 
-	backSpeed: 80,
+            typeSpeed: 100,
 
-	backDelay: 1500,
-});
+            backSpeed: 80,
+
+            backDelay: 1500
+        });
 
 // ---------------------------Scroll Animation--------------------------------
 
-window.addEventListener("scroll", reveal);
-function reveal() {
-	var reveals = document.querySelectorAll(".reveal");
-	for (var i = 0; i < reveals.length; i++) {
-		var windowheight = window.innerHeight;
-		var revealtop = reveals[i].getBoundingClientRect().top;
-		var revealpoint = 10;
-		if (revealtop < windowheight - revealpoint) {
-			reveals[i].classList.add("active");
-		} else {
-			reveals[i].classList.remove("active");
-		}
-	}
-}
+    window.addEventListener('scroll', reveal);
+    function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){ 
+        var windowheight=window.innerHeight; 
+        var revealtop=reveals[i].getBoundingClientRect().top; 
+        var revealpoint=10; 
+        if(revealtop < windowheight - revealpoint) {
+        reveals[i].classList.add('active'); 
+    }else{ 
+        reveals[i].classList.remove('active'); 
+    } } } 
 
-//   ----------------------------Loader Animation ---------------------------------
 
-var loadtime;
+//   ----------------------------Loader Animation ---------------------------------  
 
-function loader() {
-	loadtime = setTimeout(showPage, 2000);
-}
+            var loadtime;
 
-function showPage() {
-	document.getElementById("loader").style.display = "none";
-	document.getElementById("main").style.display = "block";
-	document.getElementById("nav").style.display = "flex";
-}
-function showhome() {
-	var ld = setTimeout(shhome, 2100);
-	function shhome() {
-		document.getElementById("home").classList.add("active");
-	}
-}
+            function loader() {
+                loadtime = setTimeout(showPage, 2000);
+            }
 
-const date = new Date();
-const year = date.getFullYear();
-document.getElementById("copy").innerHTML = year;
+            function showPage() {
+                document.getElementById("loader").style.display = "none";
+                document.getElementById("main").style.display = "block";
+                document.getElementById("nav").style.display = "flex";
+            }
+            function showhome(){
+                var ld=setTimeout(shhome,2100)
+                function shhome(){
+                document.getElementById("home").classList.add('active');
+            }}
+
+            const date = new Date()
+                const year = date.getFullYear()
+                document.getElementById("copy").innerHTML = year
 
 // -----------------------------tilt animation --------------------------
 
 VanillaTilt.init(document.querySelector(".hero-image"), {
-	max: 25,
-	speed: 400,
+    max: 25,
+    speed: 400
 });
-VanillaTilt.init(document.querySelectorAll(".edu-box"), {
-	max: 25,
-	speed: 400,
+VanillaTilt.init(document.querySelectorAll(".edu-box"),{
+    max: 25,
+    speed: 400
 });
 VanillaTilt.init(document.querySelectorAll(".button"), {
-	max: 25,
-	speed: 400,
+    max: 25,
+    speed: 400
 });
 VanillaTilt.init(document.querySelectorAll(".skill"), {
-	max: 25,
-	speed: 400,
+    max: 25,
+    speed: 400
 });
+
 
 // ----------------------------mouse-move-------------------------
 
 function spark(event) {
-	let i = document.createElement("s");
-	i.className = "mouseTrail";
-	// Set the position of the element based on the mouse event
-	i.style.left = event.pageX + "px";
-	i.style.top = event.pageY + "px";
-
-	// Randomly scale the element
-	i.style.transform = `scale(${Math.random() * 2 + 1})`;
-
-	// Set random transition values
-	i.style.setProperty("--x", getRandomTransitionValue());
-	i.style.setProperty("--y", getRandomTransitionValue());
-	document.body.appendChild(i);
-
-	// Remove the element after 2 seconds
-	setTimeout(() => {
-		document.body.removeChild(i);
-	}, 2000);
-}
-
-function getRandomTransitionValue() {
-	// Generate a random value between -200 and 200 pixels
-	return `${Math.random() * 400 - 200}px`;
-}
-
-// Add event listener to track mouse movements and create spark effect
-document.addEventListener("mousemove", spark);
-
-//   ----------------------------Scroll Top---------------------------------
-
-const scrollTopBtn = document.querySelector(".js-scroll-top");
-if (scrollTopBtn) {
-	scrollTopBtn.onclick = () => {
-		window.scrollTo({ top: 0, behavior: "smooth" });
-	};
-
-	const progressPath = document.querySelector(".scroll-top path");
-	const pathLength = progressPath.getTotalLength();
-	progressPath.style.transition = progressPath.style.WebkitTransition = "none";
-	progressPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
-	progressPath.style.strokeDashoffset = pathLength;
-	progressPath.getBoundingClientRect();
-	progressPath.style.transition = progressPath.style.WebkitTransition =
-		"stroke-dashoffset 10ms linear";
-	const updateProgress = function () {
-		const scroll =
-			window.scrollY ||
-			window.scrollTopBtn ||
-			document.documentElement.scrollTopBtn;
-
-		const docHeight = Math.max(
-			document.body.scrollHeight,
-			document.documentElement.scrollHeight,
-			document.body.offsetHeight,
-			document.documentElement.offsetHeight,
-			document.body.clientHeight,
-			document.documentElement.clientHeight
-		);
-
-		const windowHeight = Math.max(
-			document.documentElement.clientHeight,
-			window.innerHeight || 0
-		);
-
-		const height = docHeight - windowHeight;
-		var progress = pathLength - (scroll * pathLength) / height;
-		progressPath.style.strokeDashoffset = progress;
-	};
-
-	updateProgress();
-	const offset = 100;
-
-	window.addEventListener(
-		"scroll",
-		function (event) {
-			updateProgress();
-
-			//Scroll back to top
-			const scrollPos =
-				window.scrollY ||
-				window.scrollTopBtn ||
-				document.getElementsByTagName("html")[0].scrollTopBtn;
-			scrollPos > offset
-				? scrollTopBtn.classList.add("is-active")
-				: scrollTopBtn.classList.remove("is-active");
-		},
-		false
-	);
-}
-
-//   ----------------------------Lock Right-Click---------------------------------
-
-document.addEventListener("contextmenu", function (e) {
-	e.preventDefault();
-});
-
-document.addEventListener("selectstart", function (e) {
-	e.preventDefault();
-});
+    let i = document.createElement("s");
+    i.className = "mouseTrail";
+    // Set the position of the element based on the mouse event
+    i.style.left = event.pageX + "px";
+    i.style.top = event.pageY + "px";
+  
+    // Randomly scale the element
+    i.style.transform = `scale(${Math.random() * 2 + 1})`;
+  
+    // Set random transition values
+    i.style.setProperty("--x", getRandomTransitionValue());
+    i.style.setProperty("--y", getRandomTransitionValue());
+    document.body.appendChild(i);
+  
+    // Remove the element after 2 seconds
+    setTimeout(() => {
+      document.body.removeChild(i);
+    }, 2000);
+  };
+  
+  function getRandomTransitionValue() {
+    // Generate a random value between -200 and 200 pixels
+    return `${Math.random() * 400 - 200}px`;
+  }
+  
+  // Add event listener to track mouse movements and create spark effect
+  document.addEventListener("mousemove", spark);
+  
