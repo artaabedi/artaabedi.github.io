a2a5e67ee4439668bfd16f25f1edb48999b45faf
Arta Abedi Firouzjaei <Hey@artaabedi.com>
2024-09-23 17:35:50 +0330

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -207,3 +207,16 @@ document.addEventListener("selectstart", function (e) {
 	e.preventDefault();
 });
 
+
+//   ----------------------------Google Tag---------------------------------
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=GT-55X3VN32"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'GT-55X3VN32');
+</script>
+
